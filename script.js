f2ba10d96023e3c9e4cb6e49eba6ab020206d9ff
hencci <hencci20@gmail.com>
2025-02-12 21:30:03 +0100

    Create functions to reset board, and add mark to cell

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
 const Gameboard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];  // An array representing the grid
 
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";  // Reset every spot on the board
+        }
+    };
+
+    const getBoard = () => board;
+
+    const setMark = (index, mark) => {
+        if (board[index] === "") {  // Ensure the spot is empty
+            board[index] = mark;
+        }
+    };
 })();
